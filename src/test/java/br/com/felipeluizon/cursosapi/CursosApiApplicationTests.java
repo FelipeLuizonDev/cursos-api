1da7b0ce88ea949bcdaf1a84dd6b8745aa5b98e1
Felipe Luizon <luizonfelipe@gmail.com>
2026-05-26 09:53:06 -0300

    chore: initial spring boot setup

---
+++ src/test/java/br/com/felipeluizon/cursosapi/CursosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeluizon.cursosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
